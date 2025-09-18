a76d71aa0975cda9bda0efe988161325ab84dd88
Anish <anishnbaskaran@gmail.com>
2025-09-18 20:26:56 +0100

    Implemented decode logic for decompression

--- decode.cpp
+++ decode.cpp
@@ -7,6 +7,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "huffman.h"
+
 int parseSize(std::string& inputFile) {
   std::ifstream input(inputFile, std::ios::binary);
   if (!input) {
@@ -29,3 +31,44 @@ std::unordered_map<unsigned char, int> parseFreqTable(std::string& inputFile) {
   }
   return freqs;
 }
+
+void decode(std::string& inputFile, std::string& outputFile, Node *root, int origSize) {
+  std::ifstream input(inputFile, std::ios::binary);
+  if (!input) {
+    std::cerr << "Error opening file " << inputFile << std::endl;
+    return;
+  }
+  std::ofstream output(outputFile, std::ios::binary);
+  if (!output) {
+    std::cerr << "Error creating file " << outputFile << std::endl;
+    return;
+  }
+  unsigned char byte;
+  int bitCount = 0;
+  int size = 0;
+  while (size < origSize) {
+    Node *curr = root;
+    while (curr->left || curr->right) {
+      if (bitCount == 0) {
+        input.read(reinterpret_cast<char *>(&byte), 1);
+        bitCount = 8;
+      }
+      int bit = (byte >> (bitCount-1)) & 0x1;
+      if (bit) {
+        curr = curr->right;
+      } else {
+        curr = curr->left;
+      }
+      bitCount--;
+    }
+    output.write(reinterpret_cast<char *>(&curr->c), 1);
+    size++;
+  }
+  input.close();
+  output.close();
+}
+
+// size = parseSize(inputFile);
+// freqs = parseFreqTable(inputFile);
+// Node *root = buildHuff(freqs);
+// decode(inputFile, outputFile, root);
--- decode.h
+++ decode.h
@@ -4,8 +4,10 @@
 #include <string>
 #include <unordered_map>
 
+#include "huffman.h"
+
 int parseSize(std::string& inputFile); // get size of the original size in number of symbols.
 std::unordered_map<unsigned char, int> parseFreqTable(std::string& inputFile); // freq table parsed
-void decode(std::string& inputFile, std::string& outputFile); // decode using tree traversal
+void decode(std::string& inputFile, std::string& outputFile, Node *root, int size); // decode using tree traversal
 
 #endif //DECODE_H
